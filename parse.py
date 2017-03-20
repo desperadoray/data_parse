872036c341b49365f3688d752c20ae5ad1286565
Ray <desperadoray@users.noreply.github.com>
2017-03-20 13:36:35 -0700

    Update parse.py

--- parse.py
+++ parse.py
@@ -4,6 +4,7 @@ import re
 
 maxInt = sys.maxsize
 decrement = True
+newrows = []
 
 while decrement:
     # decrease the maxInt value by factor 10 
@@ -16,12 +17,28 @@ while decrement:
         maxInt = int(maxInt/10)
         decrement = True
 
-with open(r'D:\Coding\hausway\database\bca\2017Feb\Monthly Sales Report.csv', newline = '') as csvfile:
+path = input()
+
+with open(r'D:\Coding\hausway\database\bca\2017Feb\Monthly Sales Report2.csv', 'r', encoding = 'utf-8') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter = '\t')
 	for row in spamreader:
+		newrow = row
 		r = re.search(r'^UNIT\s([0-9]+)\s([0-9]+)', row[7])
 		if(bool(r)):
 			unitNumber = r.group(1)
 			streetNumber = r.group(2)
+			newrow.append(unitNumber)
+			newrow.append(streetNumber)
+		else:
+			newrow.append('')
+			newrow.append('')
 		if(row[18] == "Width * Depth"):
 			size = float(row[19])*float(row[20])
+			newrow[19] = size
+			newrow[20] = ''
+			newrow[18] = 'Square Feet'
+		newrows.append(row)
+
+with open(r'D:\coding\hausway\database\test1.csv', 'a+', newline = '') as f:
+	writer = csv.writer(f)
+	writer.writerows(newrows)
